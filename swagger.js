625e8c385992a6127e8b6a6904808b2f32e78169
saikrishnayadav764 <106056182+saikrishnayadav764@users.noreply.github.com>
2024-05-01 23:14:22 +0530

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "https://task-management-api-dk0b.onrender.com/",
+        url: "https://task-management-api-klht.onrender.com/",
         description: "Live server",
       },
     ],
